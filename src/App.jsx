da6dd043049fdfd83905dd84bad06c1a67e77d4f
Gr3g03 <71180086+Gr3g03@users.noreply.github.com>
2022-01-24 15:20:43 +0100

    working with keypress

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,21 @@ function App() {
 
   let words = ['bench', 'advance', 'potato', 'medieval', 'aluminium', 'circulation', 'mercy']
 
-  // const [word1, setword1] = useState(
-  //   {
-  //     word: getRandomWord(words)
-  //   })
+
+  const [letter, setletter] = useState(getRandomWord(words))
+
+  function KeyPressElement() {
+    function handleKeyPress() {
+      console.log("You pressed a key.")
+    }
+    return (
+      <div>
+        <input type="text" onKeyPress={(e) => handleKeyPress(e)} />
+      </div>
+    )
+  }
+
+  console.log(KeyPressElement())
 
   function getRandomWord(words) {
     const randomWord = Math.floor(Math.random() * words.length)
@@ -19,7 +30,8 @@ function App() {
 
   return (
     <div className="App">
-      <span>{getRandomWord(words)}</span>
+
+      <span>{letter}</span>
 
     </div>
   )
