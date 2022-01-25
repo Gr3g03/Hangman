b117b4d4e38acd1f784954be9e40487795034566
Gr3g03 <71180086+Gr3g03@users.noreply.github.com>
2022-01-25 15:09:04 +0100

    added some more words

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,109 @@ import './App.css'
 
 function App() {
 
-  let words = ['bench', 'advance', 'potato', 'medieval', 'aluminium', 'circulation', 'mercy']
+  const letters = 'abcdefghijklmnopqrstuvwxyz'
+
+  let words =
+    ['bench',
+      'advance',
+      'potato',
+      'medieval',
+      'aluminium',
+      'circulation',
+      'mercy',
+      'execution',
+      'deteriorate',
+      'arrangement',
+      'banquet',
+      'employ',
+      'start',
+      'dollar',
+      'timetable',
+      'secretary',
+      'prisoner',
+      'glasses',
+      'letter',
+      'sanctuary',
+      'current',
+      'arena',
+      'budge',
+      'escape',
+      'participate',
+      'snail',
+      'excavate',
+      'powder',
+      'leash',
+      'circulation',
+      'master',
+      'plaintiff',
+      'suppress',
+      'chain',
+      'center',
+      'sight',
+      'necklace',
+      'block',
+      'guide',
+      'bless',
+      'suffering',
+      'evolution',
+      'forecast',
+      'hospitality',
+      'employee',
+      'referee',
+      'portrait',
+      'chocolate',
+      'dismissal',
+      'thank',
+      'motif',
+      'learn',
+      'custody',
+      'benefit',
+      'vacuum',
+      'index',
+      'financial'
+    ]
 
 
-  const [letter, setletter] = useState(getRandomWord(words))
-  // const [keyPress, SetkeyPress] = useState('')
-  const [guesses, setGuess] = useState([])
+  const [word, setWord] = useState(getRandomWord(words))
+  const [guesses, setGuesses] = useState([])
+
+
+
+  const rightGuesses = guesses.filter(guess => word.includes(guess))
+
+  const wrongGuesses = guesses.filter(guess => !word.includes(guess))
+
+  const lives = 5 - wrongGuesses.length
+
+  const won = word.split('').every(char => rightGuesses.includes(char))
+
+
+  const lost = lives === 0
+
+
+  function reset() {
+    setGuesses([])
+    setWord(getRandomWord())
+  }
 
 
 
   useEffect(() => {
-    document.addEventListener('keypress', function (e) {
+    if (lost || won) return
 
-      if (!guesses.includes(e.key)) {
+    const listener = e => {
+      const guess = e.key.toLowerCase()
+      if (!letters.includes(guess)) return
 
-        setGuess(e.key)
-      }
+      if (guesses.includes(guess)) return
 
-    })
-  }, [])
+      setGuesses([...guesses, guess])
+    }
+
+    window.addEventListener('keydown', listener)
+
+    return () => window.removeEventListener('keydown', listener)
+  }, [guesses, lost, won])
 
 
   function getRandomWord(words) {
@@ -30,27 +114,31 @@ function App() {
   }
 
 
-  function rightGuesses() {
-    return guesses.filter(function (word) {
-      return letter.includes(word)
-    })
-  }
-
-  function wrongGuesses() {
-    return guesses.filter(function (word) {
-      return !letter.includes(word)
-    })
-  }
-
-  // const allWrongGuesses = wrongGuesses()
-
 
   return (
     <div className="App">
 
-      <span>{letter}</span>
-      <span> {wrongGuesses.length} </span>
+      <div className='RightGuess'>
+        {word.split('').map((char, index) => (
+          <span key={index}>{rightGuesses.includes(char) ? char : '_'}</span>
+        ))}
+      </div>
+      <p>Wrong guesses: {wrongGuesses}</p>
+      <p>Lives: {lives}</p>
+      {lost ? (
+        <div>
+          <p>You lost </p>
+          <p>The word was: {word}</p>
+          <button onClick={reset}>RESET</button>
+        </div>
+      ) : null}
 
+      {won ? (
+        <div>
+          <p>You won! </p>
+          <button onClick={reset}>RESET</button>
+        </div>
+      ) : null}
     </div>
   )
 }
